647be376115b272656a04696c47f7263cc40dd55
nqdinh1203 <dinh.nguyen@gihot.vn>
2023-07-12 18:24:17 +0700

    solving exercise 2...

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, Rust Bootcamp by VBI Academy!");
+    let values: Vec<f64> = vec![
+        2817.42, 2162.17, 3756.57, 2817.42, -2817.42, 946.9, 2817.42, 964.42, 795.43, 3756.57,
+        139.34, 903.58, -3756.57, 939.14, 828.04, 1120.04, 604.03, 3354.74, 2748.06, 1470.8,
+        4695.71, 71.11, 2391.48, 331.29, 1214.69, 863.52, 7810.01,
+    ];
+
+    let values_number = values.len();
+    println!("{:?}", values);
+
+    let additions: Vec<usize> = vec![0];
+
+    println!("{:?}", values_number);
+
+    while additions.len() as u32 > 0 {
+        let mut addition: f64 = 0.0;
+
+        // Sumar valores en additions
+        for element_index in &additions {
+            let addition_aux = values[*element_index];
+            addition += addition_aux;
+        }
+    }
 }
